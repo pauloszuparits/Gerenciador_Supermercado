2827c4e45e74b256b5e7d2e168c04b778d3bb7c2
Paulo Szuparits <pauloszuparits@hotmail.com>
2022-11-23 23:18:30 -0300

    Finished the function cadastrar produto and it is in progress the main menu

--- code.py
+++ code.py
@@ -1,8 +1,51 @@
 import mysql.connector
 from mysql.connector import errorcode
+import datetime
 
-db_connection = mysql.connector.connect(host='localhost', user='root', password='Oicueio1!@#$', database='supermercado')
+# Funções
+
+# Insere produto
+def cadastrarProduto(nome, peso, valor):
+    db_connection = mysql.connector.connect(host='localhost', user='root', password='Oicueio1!@#$', database='supermercado');
+
+    cursor = db_connection.cursor()
+
+    sql = "INSERT INTO Produtos(Nome, Peso, Valor) VALUES (%s, %s, %s)";
+    values = (nome, peso, valor);
+
+    try:
+        cursor.execute(sql, values);
+
+        cursor.close()
+        db_connection.commit()
+        db_connection.close()
+    except:
+        print("Erro na inserção");
+        cursor.close()
+        db_connection.close()
+        return;
+
+    
 
 
+# Execução
 
-db_connection.close()
+print("Seja bem vindo ao sistema do supermercado!");
+print("Escolha uma das opções abaixo para prosseguir");
+
+
+op = input(
+    "1 - Cadastrar Produto\n"
+    "2 - Procurar Produto\n"
+    "3 - Alterar Produto\n"
+    "4 - Remover Produto\n"
+);
+
+if op == "1":
+    nome = input("Digite o nome do produto: \n");
+         
+    peso = float(input("Digite o peso do produto: \n"));
+    valor = float(input("Digite o valor do produto: \n"));
+       
+    cadastrarProduto(nome, peso, valor);
+    
